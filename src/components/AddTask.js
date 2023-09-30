63fa10f0437160608075bc6e48f107a000ea3c4a
aqibrafique <aqibrafique97@gmail.com>
2023-09-30 12:53:46 +0500

    sep 30

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import Style from './Style.module.css'
 
 function AddTask(props) {
  
   return (
     <>
-    <div style={{width:"99%", height:"200px", border: "solid 1px red", display:"flex", justifyContent:"center", alignItems:"center" }}>
-        <div style={{width: "50%", height:"100px", border: "solid 1px blue" }}>
-            <input style={{width:"60%", height:"50px", background:"#DFAA82"}} stype type='text' value={props.todo} onChange={(e)=>{props.setTodo(e.target.value)}} />
+    <div style={{color:"yellow",fontSize: "60px",fontFamily:"cursive", textAlign:"center" }}>Todo App</div>
+    <div style={{width:"100%", height:"150px", border: "solid 1px red", display:"flex", justifyContent:"center", alignItems:"center" }}>
+      
+        {/* <div style={{width: "50%", height:"100px", border: "solid 1px blue" }}> */}
+            <input style={{width:"50%", height:"50px", background:"white", borderRadius:"15px", border:"4px dotted yellow" }} stype type='text' value={props.todo} onChange={(e)=>{props.setTodo(e.target.value)}} />
 
-            <button onClick={props.AddTodo}>Add</button>
+            <button className={Style.addbutton} style={{marginLeft:"10px", height:"60px", backgroundColor:"yellow", color:"red",fontSize:"25px", borderRadius:"25px", border:"none" }} onClick={props.AddTodo}>Add</button>
             
 
-        </div>
+        {/* </div> */}
     </div>
     </>
   )
--- src/components/DisplayTodo.js
+++ src/components/DisplayTodo.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import DisplayAdd from './DisplayAdd';
-
 const DisplayTodo = (props) => {
     console.log("display"+ props.todoList.length);
-    // console.log("id is"+)
-//   const list =  props.todoList.filter((e)=>{if(e.Status=="add"){return e}}).map((e)=>{return(<div>{e.todo}</div>)});
-
   return(
-    <div style={{border:"solid 1px blue", height:"400px", width:"100%", display:"flex", justifyContent:"space-around", alignItems:"center"}}>
-        <div style={{border:"solid 1px red", height:"300px", width:"30%", display:"", justifyContent:"center"}}>
+    <div style={{border:"solid 1px blue", height:"400px", width:"100%", display:"flex", justifyContent:"space-around", alignItems:"center", flexWrap:"wrap"}}>
+        <div style={{border:"solid 3px green", height:"300px", width:"30%"}}>
+        <div style={{ border: "solid 1px red", height: "40px", width: "80%", display:"flex", justifyContent:"center", alignItems:"center",color:"yellow",fontSize: "30px", fontFamily:"cursive"}}>All Tasks</div>
        { props.todoList.filter((e)=>{if(e.Status==="add"){return e}}).map((e)=>
        <DisplayAdd
        todoList={props.todoList}
@@ -18,7 +15,6 @@ const DisplayTodo = (props) => {
        Status={e.Status}
        />
        )
-       
        }
         </div>
         <div style={{border:"solid 1px red", height:"300px", width:"30%"}}>
@@ -49,8 +45,7 @@ const DisplayTodo = (props) => {
        }
             
         </div>  
-   
-    </div>
+        </div>
   )
 }
 
